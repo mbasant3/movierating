97c5d6368c79b34c83a63969c39f89d5a461b803
Basant Mandal <basant@onepercentstartups.com>
2023-05-08 11:47:59 +0530

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import PatientLineChart from "./components/patientLine";
-// import App from './App'
-// import  Api  from "./components/Api";
-// import Table from "./pages/Table";
-// import ChartComponent from "./components/chartComponent";
+// import PatientLineChart from "./components/patientLine";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<PatientLineChart />);
+root.render(<App />);
